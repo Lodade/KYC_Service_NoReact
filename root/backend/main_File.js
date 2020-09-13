921be3d39962a4e064171cf0b329ba8679344748
Jacob Wagner <jwagner472@gmail.com>
2020-09-13 17:02:52 -0400

    file upload to backend added

--- root/backend/main_File.js
+++ root/backend/main_File.js
@@ -1,2 +1,18 @@
 const express = require("express");
 const service = express();
+const upload =  require("express-fileupload");
+service.use(upload());
+service.listen(3000);
+console.log("Listening on port 3000");
+
+service.get("/test", function(req,res){
+    
+});
+service.post("/test", function(req,res){
+    if(req.files){
+        var file = req.files.xmlFileUpload;
+        console.log(file);
+        file.mv("./uploads/" + file.name);
+        res.send("File received!");
+    }
+});
